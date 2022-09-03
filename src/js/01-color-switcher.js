e09124c37b64585f5800ab20975db6f615eb3798
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-09-03 07:21:40 +0300

    finish task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+stopBtn.setAttribute("disabled", "disabled");
+
+function onStartBtn(e) {
+    timerId = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;     
+    }, 1000);    
+    startBtn.setAttribute("disabled", "disabled");
+    stopBtn.removeAttribute("disabled", "disabled");
+}
+
+function onStopBtn(e) {
+    clearInterval(timerId);
+    stopBtn.setAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled", "disabled");
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
